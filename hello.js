cb57b39e67e34ec87bd49d7c70ac9beef40dc869
Aizachan <janlayaiza@gmail.com>
2025-08-29 08:41:13 +0800

    Added initial arithmetic - addition

--- hello.js
+++ hello.js
@@ -5,3 +5,14 @@ no2 = 3;
 sum = no1 + no2;
 
 console.log("The sum of: " + no1 + " and " + no2 + " is " + sum);
+
+diff = num1 + num2;
+prod = num1 * num2;
+quo = num1 / num2;
+
+console.log("the sum of "+ num1+" & "+num2+" is "+sum);
+console.log("the difference of "+num1+" & "+num2+" is "+diff);
+console.log("the product of "+ num1+" & "+num2+" is "+prod);
+console.log("the quotient of "+ num1+" & "+num2+" is "+quo);
+
+
